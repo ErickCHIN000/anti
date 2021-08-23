3239d7ad03391a607d9a0db323e4bd054d9c987d
ErickCHIN000 <61078393+ErickCHIN000@users.noreply.github.com>
2021-08-23 02:03:34 -0400

    Add files via upload

--- anti.js
+++ anti.js
@@ -1,33 +1,25 @@
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
+let count;
+
+function con(){
+  console.log(Cookies.get());
+}
+
+function save() {
+  Cookies.set('count', count);
+  display_anti();
+}
   
-  function getCookie(cname) {
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  
-  function checkCookie() {
-    let user = getCookie("username");
-    if (user != "") {
-      alert("Welcome again " + user);
-    } else {
-      user = prompt("Please enter your name:", "");
-      if (user != "" && user != null) {
-        setCookie("username", user, 365);
-      }
-    }
-  }
+function load(){
+  count = Cookies.get('count');
+  display_anti();
+}
+
+function clear(){
+  //Cookies.remove('count');
+  Cookies.remove('count', { path: '' })
+  display_anti();
+}
+
+function display_anti(){
+  document.getElementById("count").innerHTML = "Count: " + count;
+}
